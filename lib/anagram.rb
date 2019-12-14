b45d15b7d85152d07bc542c38a9ee023584fe275
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-14 16:13:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -18,11 +18,11 @@ class Anagram
     array.each do |x|
     if x.split("").sort == @word.split("").sort
       @@all_matches << x
-    end 
+    end
     return @@all_matches
     binding.pry
   end
-   
+
   end
 
 end
