ca4e5885cbaa16f8801a8ce2e6e9be5148970b03
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-14 16:10:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,10 +3,15 @@ require 'pry'
 class Anagram
     attr_accessor :word, :array
 
+    @@all_matches
 
   def initialize(word)
     @word = word
   end
+
+  def all_matches
+    @@all_matches
+  end 
 
 
   def match(array)
